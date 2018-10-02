6669ccf7e8b6d7606482e08c22ca5d829ec85445
Yi Xie <yixie@u.boisestate.edu>
2018-10-02 09:36:05 -0600

    add admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,12 +2,25 @@
 
 @section('title')
 
-    About us
+    Login
 
 @endsection
 
 @section('content')
-
-    <h1> Admin page</h1>
-
-@endsection
+    <div class="container mt-5 mb-5 ">
+        <div class="row justify-content-md-center">
+            <div class="col col-lg-2">
+            </div>
+            <div class="col-md-auto ">
+            <div class="card  shadow" style="width: 40rem; background-color: #e1f5fe;">
+                <div class="card-body ">
+                    @include('inc.adminform')
+                </div>
+            </div>
+            </div>
+            <div class="col col-lg-2">
+            
+            </div>
+        </div>
+</div>
+@stop
--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -4,7 +4,7 @@
             <p style="font-size:36px"><span style="color: #d9534f">Ashby Cabinetry </span> Inc.</p>
         </div>
         <div class="col-md-6 text-right">
-        <button type="button" class="btn btn-primary mt-3" onclick="location.href='{{ url('/admin') }}'"><span class="cui-contrast"></span> Admin Access</button>
+        <button type="button" class="btn btn-primary mt-3" onclick="location.href='{{ url('/admin') }}'"><span class="cui-contrast"></span> Login</button>
         
         </div>
     </div>
