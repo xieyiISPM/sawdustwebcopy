7bc31f269462323267e187a1f4498bb86c057ea7
Yi Xie <YIXIE@boisestate.edu>
2018-10-11 22:01:40 -0600

    sslmode=require

--- config/database.php
+++ config/database.php
@@ -82,7 +82,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
             
         ],
 
